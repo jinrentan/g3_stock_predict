5814c210c28fe8b4956b5c25d93ef2c2c2c3fd67
Jin Ren <94114901+jinrentan@users.noreply.github.com>
2024-05-24 23:12:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from stock_analysis import load_data, calculate_indicators, plot_stock, get_stock_list
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+def download_dataset():
+    # Initialize Kaggle API client and authenticate using secrets
+    api = KaggleApi()
+    api.set_config_value('username', st.secrets["kaggle"]["username"])
+    api.set_config_value('key', st.secrets["kaggle"]["key"])
+    api.authenticate()
+    
+    # Define the dataset and the path where files will be downloaded
+    dataset = 'borismarjanovic/price-volume-data-for-all-us-stocks-etfs'
+    path = 'dataset'
+
+    # Download the dataset
+    api.dataset_download_files(dataset, path=path, unzip=True)
 
 # Load data
 
